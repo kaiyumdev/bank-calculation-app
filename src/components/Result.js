d2c2a7535fa10f1b50b4df2751f4b0efe1f98474
MdKaioumislam5 <afrankaiyum34@gmail.com>
2023-08-09 11:00:40 +0600

    [DONE]finally complete the project

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,7 +1,101 @@
-import React from "react";
+// import { Stack, Typography } from "@mui/material";
+// import React from "react";
+// import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+// import { Pie } from "react-chartjs-2";
 
-const Result = () => {
-  return <div>Result</div>;
+// ChartJS.register(ArcElement, Tooltip, Legend);
+
+// const Result = ({ data }) => {
+//   // console.log(data.interestRate);
+//   // console.log(data.loanAmount);
+//   const { homeValue, loanAmount, loanTerm, interestRate } = data;
+
+//   const totalLoanMonths = loanTerm * 12;
+//   const interestPerMonth = interestRate / (100 / 12);
+
+//   const monthlyPayment =
+//     (loanAmount * interestPerMonth(1 + interestPerMonth) ** totalLoanMonths) /
+//     ((1 + interestPerMonth) ** totalLoanMonths - 1);
+
+//   const totalInterestGenerated = monthlyPayment * totalLoanMonths - loanAmount;
+
+//   const pieChartData = {
+//     labels: ["Principles", "Interest"],
+//     datasets: [
+//       {
+//         label: "Ratio of Principal and Interest",
+//         data: [homeValue, totalInterestGenerated],
+//         backgroundColor: ["rgba(255, 99, 132, 0.2)", "rgba(54, 162, 235, 0.2)"],
+//         borderColor: ["rgba(255, 99, 132, 1)", "rgba(54, 162, 235, 1)"],
+//         borderWidth: 1,
+//       },
+//     ],
+//   };
+
+//   return (
+//     <Stack>
+//       <Typography textAlign="center" variant="h5">
+//         Monthly Payment: ${monthlyPayment.toFixed(2)}
+//       </Typography>
+//       <Stack direction="row" justifyContent="center">
+//         <div>
+//           <Pie data={pieChartData}></Pie>
+//         </div>
+//       </Stack>
+//     </Stack>
+//   );
+// };
+
+// export default Result;
+
+import { Stack, Typography } from "@mui/material";
+import React from "react";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import { Pie } from "react-chartjs-2";
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+const Result = ({ data }) => {
+  // console.log(data.interestRate);
+  // console.log(data.loanAmount);
+  const { homeValue, loanAmount, loanTerm, interestRate } = data;
+
+  const totalLoanMonths = loanTerm * 12;
+  const interestPerMonth = interestRate / (100 / 12);
+
+  const monthlyPayment =
+    (loanAmount *
+      interestPerMonth *
+      (1 + interestPerMonth) ** totalLoanMonths) /
+    ((1 + interestPerMonth) ** totalLoanMonths - 1);
+
+  const totalInterestGenerated = monthlyPayment * totalLoanMonths - loanAmount;
+
+  const pieChartData = {
+    labels: ["Principles", "Interest"],
+    datasets: [
+      {
+        label: "Ratio of Principal and Interest",
+        data: [homeValue, totalInterestGenerated],
+        backgroundColor: ["rgba(255, 99, 132, 0.2)", "rgba(54, 162, 235, 0.2)"],
+        borderColor: ["rgba(255, 99, 132, 1)", "rgba(54, 162, 235, 1)"],
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  return (
+    <Stack>
+      <Typography textAlign="center" variant="h5">
+        Monthly Payment: ${monthlyPayment.toFixed(2)}
+      </Typography>
+      <Stack direction="row" justifyContent="center">
+        <div>
+          <Pie data={pieChartData}></Pie>
+        </div>
+      </Stack>
+    </Stack>
+  );
 };
 
 export default Result;
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import { theme } from "./theme";
 
